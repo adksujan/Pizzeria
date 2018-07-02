7c7b7ea8eea25f1d9b9fbdcf31a0d59b6f30b18c
Sujan <sujan.off@gmail.com>
2018-07-02 12:09:54 +0200

    Created Toppings and Pizza models, added it to admin site, made migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    veg = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return self.name
