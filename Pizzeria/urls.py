37057468aa01b819960395ced2d787d4cd0cb399
Sujan <sujan.off@gmail.com>
2018-07-04 13:32:03 +0200

    Added menu in the root

--- Pizzeria/urls.py
+++ Pizzeria/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import PizzaMenu
 
 urlpatterns = [
+    path('', PizzaMenu.as_view(), name='pizzamenu' ),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
