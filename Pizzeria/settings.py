f724860bb6c13e15bdac2796d80751e4c7d40b01
Sujan <sujan.off@gmail.com>
2018-07-04 13:51:00 +0200

    switch to development mode

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4%@)$wolm!qd*v-qqj=dr6svr78-04pyccl7()f2gy=z#pb!58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
